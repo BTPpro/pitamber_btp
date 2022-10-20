4f6942d34beb917b14e3b40c16eba10255ee278e
Pitamber26 <106551793+Pitamber26@users.noreply.github.com>
2022-10-21 00:09:16 +0530

    initial

---
+++ business/src/App.js
@@ -0,0 +1,14 @@
+
+
+import EditProfile from "./components/EditProfile";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+     <EditProfile />
+    </div>
+  );
+}
+
+export default App;
